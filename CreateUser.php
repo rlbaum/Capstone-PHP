7646d482c339d56e9662cb73894b531f527b75c4
jtadamec <jamestadamec@gmail.com>
2014-02-03 23:05:56 -0700

    Removed random number generator from query, as the database will take care of assigning a unique user id for us.

--- CreateUser.php
+++ CreateUser.php
@@ -1,5 +1,4 @@
-
-<?php\
+<?php
 // This page, CreateUser.php, generates a user ID for an inputted 
 // f_name, l_name and email.
 
@@ -7,9 +6,8 @@ $connection = mysql_connect("localhost", "root", "PASSWORD");
 //This query will insert a new user's ID and information into the user's
 //table
 
-$query = "INSERT INTO Users (user_id, email, f_name, l_name, timezone) 
-          VALUES ( uniqid (rand (),true), $_GET["f_name"], $_GET["l_name"], "
-         ."NULL".")";
+$query = "INSERT INTO Users (email, f_name, l_name) 
+          VALUES (".$_GET["email"].",".$_GET["f_name"].",".$_GET["l_name"].")";
 
  if (!$connection) {
     echo "Connection to database failed: ". mysql_error();
@@ -19,7 +17,6 @@ if (!mysql_select_db("Capstone")) {
     echo "Unable to select Capstone: ".mysql_error();
     exit;
 }
-
 
 //Now we try to add the new user to the Users table and return
 //success if it works
