56b20e229c44aca4d6b93d62b27ad653794da0ce
rlbaum <rlbaum3@gmail.com>
2014-03-04 20:09:21 -0700

    Update AttendMeeting.php
    I added in a return on success the timeslice for the meeting represented by start and end. This way you can run AddUnavail.php after this script to achieve the functionality described on drive. -Rob

--- AttendMeeting.php
+++ AttendMeeting.php
@@ -1,11 +1,13 @@
 <?php
 /*This page, AttendMeeting.php, is used to update the visible name associated to a user id. 
  * It requires 2 arguments: meeting_id and user_id
+ * Returns on success the start and end times to run in the AddUnavail.php script
  */
 
 $connection = mysql_connect("localhost", "root", "PASSWORD");
 //This query will give us the user id associated with an email
 $query="INSERT INTO Attendees (meeting_id, user_id) VALUES (".$_GET["meeting_id"].", ".$_GET["user_id"].")";
+$timeSE="SELECT m.start, m.end FROM Meetings m WHERE m.meeting_id=".$_GET["meeting_id"].")";
 if (!$connection) {
     echo "Connection to database failed: ".mysql_error();
     exit;
@@ -22,7 +24,15 @@ if (!$result) {
     exit;
 }
 else {
-    echo "Success\n";
+    $result2 = mysql_query($timeSE);
+    if(!$result2){
+        echo "Unable to obtain time slice \"".$query2." from database: ".mysql_error();
+    }
+    else{
+        echo $result2["start"]."\n";
+        echo $result2["end"]."\n";
+    }
 }
 mysql_free_result($result);
+mysql_free_result($result2);
 ?>}
